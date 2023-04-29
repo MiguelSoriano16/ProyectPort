f60571cde8dc850ac7fdf6b2f977e952ec919282
MiguelSoriano16 <Miguel.Soriano@alu.uclm.es>
2023-04-29 17:42:54 +0200

    commit message

--- src/PortManagement.java
+++ src/PortManagement.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -38,6 +39,8 @@ public class PortManagement extends JFrame{
     private JRadioButton hub1radiobutton;
     private JRadioButton hub2RadioButton;
     private JRadioButton hub3radiobuttom;
+    private JLabel Total;
+    private JLabel IDTOSHOW;
     Port port=new Port();
     int hub=1;
 
@@ -47,88 +50,117 @@ public class PortManagement extends JFrame{
     public PortManagement(){
         setContentPane(MainPannel);
         setTitle("Port Management");
-        setSize(1000,500);
+        setSize(1570,800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        MainPannel.setBackground(new Color(179,1,53,255));
+        IDnumber.setForeground(Color.WHITE);
+        Weight.setForeground(Color.WHITE);
+        Description.setForeground(Color.WHITE);
+        RemitentCompany.setForeground(Color.WHITE);
+        ReceiverCompany.setForeground(Color.WHITE);
+        Operations.setForeground(Color.WHITE);
+        Country.setForeground(Color.WHITE);
+        Priority.setForeground(Color.WHITE);
+        StateHubPlan.setForeground(Color.WHITE);
+        CustomInspections.setForeground(Color.WHITE);
+        IDTOSHOW.setForeground(Color.WHITE);
+        Total.setForeground(Color.WHITE);
+
 
         pilebutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Container cont=new Container();
-                int select=1;
+                Container cont = new Container();
+                int select = 1;
+                try {
                 cont.setID(Integer.parseInt(IDTextField.getText()));
                 cont.setWeight(Integer.parseInt(WeightFieldText.getText()));
                 cont.setDescription(DescriptionTextArea.getText());
-                cont.setCountry((String)CountrycomboBox1.getSelectedItem());
+                cont.setCountry((String) CountrycomboBox1.getSelectedItem());
                 cont.setCompanySend(RemitentCompanyTextField.getText());
                 cont.setCompanyReceive(ReceiverCompanyTextField.getText());
-                if(a1RadioButton.isSelected()){
-                    cont.setPriorityLevel(1);
-                }
-                if(a2RadioButton.isSelected()){
-                    cont.setPriorityLevel(2);
-                }
-                if(a3RadioButton.isSelected()){
-                    cont.setPriorityLevel(3);
-                }
-                if(CustomInspectionscheckBox1.isSelected()){
-                    cont.setInspected(true);
-                }
-                else{
-                    cont.setInspected(false);
-                }
-                if(port.hub1.stackContainer(cont)==-1){
-                    select++;
-                    hub=2;
-                    if(port.hub2.stackContainer(cont)==-1){
+                    if (a1RadioButton.isSelected()) {
+                        cont.setPriorityLevel(1);
+                    }
+                    if (a2RadioButton.isSelected()) {
+                        cont.setPriorityLevel(2);
+                    }
+                    if (a3RadioButton.isSelected()) {
+                        cont.setPriorityLevel(3);
+                    }
+                    if (CustomInspectionscheckBox1.isSelected()) {
+                        cont.setInspected(true);
+                    } else {
+                        cont.setInspected(false);
+                    }
+                    if (port.hub1.stackContainer(cont) == -1) {
                         select++;
-                        hub=3;
-                        if(port.hub3.stackContainer(cont)==-1){
+                        hub = 2;
+                        if (port.hub2.stackContainer(cont) == -1) {
                             select++;
+                            hub = 3;
+                            if (port.hub3.stackContainer(cont) == -1) {
+                                select++;
+                            }
                         }
                     }
+                    if (select == 1) {
+                        StateTextArea1.setText("Hub 1:\n" + port.hub1.stringState());
+                    }
+                    if (select == 2) {
+                        StateTextArea1.setText("Hub 2:\n" + port.hub2.stringState());
+                    }
+                    if (select == 3) {
+                        StateTextArea1.setText("Hub 3\n" + port.hub3.stringState());
+                    }
+                    IDTextField.setText("" + (cont.ID + 1));
+                    WeightFieldText.setText("");
+                    DescriptionTextArea.setText("");
+                    RemitentCompanyTextField.setText("");
+                    ReceiverCompanyTextField.setText("");
+                } catch (NumberFormatException nfe) {
+                    JOptionPane.showMessageDialog(null, "Error, fill correctly all the fields");
                 }
-                if(select==1){
-                    StateTextArea1.setText("Hub 1:\n"+port.hub1.stringState());
-                }
-                if(select==2){
-                    StateTextArea1.setText("Hub 2:\n"+port.hub2.stringState());
-                }
-                if(select==3){
-                    StateTextArea1.setText("Hub 3\n"+port.hub3.stringState());
-                }
-                IDTextField.setText(""+(cont.ID+1));
             }
         });
         showcontainerdescriptionbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!port.hub1.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")){
-                    descriptionIsShownHereTextArea.setText(port.hub1.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
-                }
-                if (!port.hub2.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")){
-                    descriptionIsShownHereTextArea.setText(port.hub2.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
-                }
-                if (!port.hub3.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")){
-                    descriptionIsShownHereTextArea.setText(port.hub3.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
-                }
+                try {
+                    if (!port.hub1.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")) {
+                        descriptionIsShownHereTextArea.setText(port.hub1.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
+                    }
+                    if (!port.hub2.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")) {
+                        descriptionIsShownHereTextArea.setText(port.hub2.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
+                    }
+                    if (!port.hub3.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())).equals("Not found")) {
+                        descriptionIsShownHereTextArea.setText(port.hub3.dataRegisteredContainer(Integer.parseInt(IDtoshow.getText())));
+                    }
+                }catch(NumberFormatException nfe){
+                    JOptionPane.showMessageDialog(null,"ID should be a number.");
+            }
             }
         });
 
         unpileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try{
                 if(hub==1){
-                    port.hub1.removeContainer(Integer.parseInt((ColumnNumberTextField.getText())));
+                    port.hub1.removeContainer(Integer.parseInt((ColumnNumberTextField.getText()))-1);
                     StateTextArea1.setText("Hub 1:\n"+port.hub1.stringState());
                 }
                 if(hub==2){
-                    port.hub2.removeContainer(Integer.parseInt((ColumnNumberTextField.getText())));
+                    port.hub2.removeContainer(Integer.parseInt((ColumnNumberTextField.getText()))-1);
                     StateTextArea1.setText("Hub 2:\n"+port.hub2.stringState());
                 }
                 if(hub==3){
-                    port.hub3.removeContainer(Integer.parseInt((ColumnNumberTextField.getText())));
+                    port.hub3.removeContainer(Integer.parseInt((ColumnNumberTextField.getText()))-1);
                     StateTextArea1.setText("Hub 3:\n"+port.hub3.stringState());
+                }
+            }catch(NumberFormatException nfe){
+                    JOptionPane.showMessageDialog(null,"Column should be a number");
                 }
             }
         });
